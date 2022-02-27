feaf24b8d3224ba2c334238f2d3bc15c7b9c1885
Swaroop Maddu <madduswaroop@gmail.com>
2022-02-27 10:28:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ const App = () => {
       if (solana) {
         if (solana.isPhantom) {
           console.log('Phantom wallet found!');
-          const response = await solana.connect({ onlyIfTrusted: true });
+          const response = await solana.connect();
           console.log(
             'Connected with Public Key:',
             response.publicKey.toString()
